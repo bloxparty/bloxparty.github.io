843a0e27fd2061ab87c664cc3951c311b7cc1bb5
Konstantin Azizov <azizovkostya97@gmail.com>
2015-11-06 10:19:02 +0200

    Restyle block animation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var shape = 0;
+var c = require('color');
 var canvas;
 var ctx;
 
@@ -29,14 +30,49 @@ function updateBlock() {
 
 function drawBlock(block) {
 	var rotation = block.rotations[Math.floor(Math.random() * 4)];
+	const cellWidth = 19;
+	const cellHeight = 19;
 	
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.fillStyle = block.color;
 	
 	for(var i = rotation.length - 1; i >= 0; i--) {
 		for(var j = rotation[i].length - 1; j >= 0; j--) {
 			if(rotation[i][j] === 1) {
-				ctx.fillRect(i * 20, j * 20, 19, 19);
+				ctx.fillStyle = block.color;
+				
+				var cellX = i * 20;
+				var cellY = j * 20;
+				var bevelWidth = cellWidth * 0.1
+				var innerX = cellX + (cellWidth * 0.1)
+				var innerY = cellY + (cellHeight * 0.1)
+				var innerWidth = cellWidth - (cellWidth * 0.1) * 2
+				var innerHeight = cellWidth - (cellHeight * 0.1) * 2
+				var topLeftColor = c(block.color).lighten(0.6).rgbString()
+				var bottomRightColor = c(block.color).darken(0.5).rgbString()
+				
+				ctx.fillRect(innerX, innerY, innerWidth, innerHeight)
+
+				ctx.fillStyle = topLeftColor
+				ctx.beginPath()
+				ctx.moveTo(cellX, cellY)
+				ctx.lineTo(cellX + cellWidth, cellY)
+				ctx.lineTo(cellX + cellWidth - bevelWidth, cellY + bevelWidth)
+				ctx.lineTo(cellX + bevelWidth, cellY + bevelWidth)
+				ctx.lineTo(cellX + bevelWidth, cellY + cellHeight - bevelWidth)
+				ctx.lineTo(cellX, cellY + cellHeight)
+				ctx.closePath()
+				ctx.fill()
+
+				ctx.fillStyle = bottomRightColor
+				ctx.beginPath()
+				ctx.moveTo(cellX + cellWidth, cellY)
+				ctx.lineTo(cellX + cellWidth, cellY + cellHeight)
+				ctx.lineTo(cellX, cellY + cellHeight)
+				ctx.lineTo(cellX + bevelWidth, cellY + cellHeight - bevelWidth)
+				ctx.lineTo(cellX + cellWidth - bevelWidth, cellY + cellHeight - bevelWidth)
+				ctx.lineTo(cellX + cellWidth - bevelWidth, cellY + bevelWidth)
+				ctx.closePath()
+				ctx.fill()
 			}
 		}
 	}
